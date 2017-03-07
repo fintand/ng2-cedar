ba1ba6b67976f3ce002b9ffe35b6429043f142f7
Fintan Dunleavy <fintan.dunleavy@gmail.com>
2017-03-07 21:19:00 +0000

    latest

--- src/app/components/pages/pricing.component.ts
+++ src/app/components/pages/pricing.component.ts
@@ -10,36 +10,16 @@ export class PricingComponent {
 
   prices = [
     {
-      lesson_type: "Lesson / Pre-Test / EDT per hour",
+      lesson_type: "Lesson | Pre-Test | EDT per hour",
       price: "30.00"
     },
     {
-      lesson_type: "Lesson + Pre-Test (2 Hours)",
-      price: "55.00"
-    },
-    {
-      lesson_type: "Pre-Test Video Recorded (Free SD Card provided)",
-      price: "35.00"
-    },
-    {
-      lesson_type: "3 Lessons",
-      price: "80.00"
-    },
-    {
       lesson_type: "6 Lessons",
-      price: "160"
+      price: "170"
     },
     {
-      lesson_type: "12 EDT Lessons (mandatory from 04-04-2011)",
-      price: "300"
-    },
-    {
-      lesson_type: "1 Hour Warm Up + Car Hire",
-      price: "100"
-    },
-    {
-      lesson_type: "2 Hour Warm Up + Car Hire",
-      price: "120"
+      lesson_type: "12 EDT Lessons",
+      price: "320"
     }
   ]
 
--- src/app/components/pages/services.component.ts
+++ src/app/components/pages/services.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
 })
 export class ServicesComponent {
 
+  services = [
+    "RSA and ADI approved instructors",
+    "Pre-test, EDT and motorway driving courses",
+    "Lessons available 7 days a week",
+    "School car available for lessons and driving test (Opel Corsa with dual controls)",
+    "Door to door pick up and drop off available",
+    "Discounted student rates",
+    "Discounts available for block booking",
+    "Gift vouchers available!",
+  ]
+
 }
