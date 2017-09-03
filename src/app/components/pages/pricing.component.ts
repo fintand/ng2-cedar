a94986f29d89d4e4e982ee70900ca44905626029
Fintan <fintan.dunleavy@celtechgroup.com>
2017-09-03 14:05:37 +0100

    Updated prices, updated build script

--- src/app/components/pages/pricing.component.ts
+++ src/app/components/pages/pricing.component.ts
@@ -16,15 +16,15 @@ export class PricingComponent {
   prices = [
     {
       lesson_type: "Lesson | Pre-Test | EDT per hour",
-      price: "30.00"
+      price: "35"
     },
     {
       lesson_type: "6 Lessons",
-      price: "170"
+      price: "190"
     },
     {
       lesson_type: "12 EDT Lessons",
-      price: "320"
+      price: "350"
     }
   ]
 
